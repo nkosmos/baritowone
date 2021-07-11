b2dd45cf23bfe2a5fe4e4099f1e11bfd43342dc4
xTrM_ <xtrmyt24@gmail.com>
2021-07-12 00:42:17 +0200

    cock

---
+++ src/api/java/baritonex/utils/XTuple.java
@@ -0,0 +1,29 @@
+package baritonex.utils;
+
+public class XTuple<A, B>
+{
+    private A a;
+    private B b;
+
+    public XTuple(A aIn, B bIn)
+    {
+        this.a = aIn;
+        this.b = bIn;
+    }
+
+    /**
+     * Get the first Object in the Tuple
+     */
+    public A getFirst()
+    {
+        return this.a;
+    }
+
+    /**
+     * Get the second Object in the Tuple
+     */
+    public B getSecond()
+    {
+        return this.b;
+    }
+}
