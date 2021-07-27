cc15f786f57763f0f5130ee70ad26ef7ffe222bd
xTrM_ <xtrmyt24@gmail.com>
2021-07-27 03:42:17 +0200

    UwU

---
+++ src/api/java/baritonex/command/defaults/DebugCommand.java
@@ -0,0 +1,47 @@
+package baritonex.command.defaults;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import baritone.api.IBaritone;
+import baritone.api.command.Command;
+import baritone.api.command.argument.IArgConsumer;
+import baritone.api.command.exception.CommandException;
+
+public class DebugCommand extends Command {
+
+    public DebugCommand(IBaritone baritone) {
+        super(baritone, "debuwug");
+    }
+
+    @Override
+    public void execute(String label, IArgConsumer args) throws CommandException {
+        args.requireMax(0);
+        
+        logDirect("You are running Baritowone >w<");
+        logDirect("version? idk <3");
+        logDirect("" + (ctx.player().getEntityBoundingBox().maxX - ctx.player().getEntityBoundingBox().minX));
+        logDirect(ctx.player().posY + " / " + ctx.player().getEntityBoundingBox().minY);
+    }
+
+    @Override
+    public Stream<String> tabComplete(String label, IArgConsumer args) {
+        return Stream.empty();
+    }
+
+    @Override
+    public String getShortDesc() {
+        return "Debug";
+    }
+
+    @Override
+    public List<String> getLongDesc() {
+        return Arrays.asList(
+                "UwU.",
+                "",
+                "Usage:",
+                "> debuwug - COCK"
+        );
+    }
+}
